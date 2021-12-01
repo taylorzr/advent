b88a7b2b7c4d38f6336679ba74202e01a8b719b7
Zach Taylor <zach@taylorzr.com>
2021-12-01 10:20:05 -0500

    Generic function that should handle any range

--- 1.go
+++ 1.go
@@ -12,15 +12,35 @@ func CountIncreasing(nums []int) int {
 	return count
 }
 
-func CountIncreasingWindow(nums []int) int {
+func CountIncreasingWindowOf3(nums []int) int {
 	count := 0
-
 	previousSum := 0
 
 	for i := 2; i < len(nums); i++ {
 		sum := nums[i] + nums[i-1] + nums[i-2]
 
 		if sum > previousSum && i != 2 {
+			count++
+		}
+
+		previousSum = sum
+	}
+
+	return count
+}
+
+func CountIncreasingWindow(nums []int, window int) int {
+	count := 0
+	previousSum := 0
+
+	for i := window - 1; i < len(nums); i++ {
+		sum := 0
+
+		for j := 0; j < window; j++ {
+			sum += nums[i-j]
+		}
+
+		if sum > previousSum && i != window-1 {
 			count++
 		}
 
--- 1_test.go
+++ 1_test.go
@@ -20,7 +20,7 @@ func TestCountIncreasing(t *testing.T) {
 	}
 }
 
-func TestCountIncreasingRange(t *testing.T) {
+func TestCountIncreasingRangeOf3(t *testing.T) {
 	tables := []struct {
 		input  []int
 		answer int
@@ -30,7 +30,28 @@ func TestCountIncreasingRange(t *testing.T) {
 	}
 
 	for i, table := range tables {
-		count := CountIncreasingWindow(table.input)
+		count := CountIncreasingWindowOf3(table.input)
+
+		if count != table.answer {
+			t.Errorf("Count was incorrect for line %d, got: %d, want: %d.", i, count, table.answer)
+		}
+	}
+}
+
+func TestCountIncreasingWindow(t *testing.T) {
+	tables := []struct {
+		input  []int
+		window int
+		answer int
+	}{
+		{[]int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}, 1, 7},
+		{input1, 1, 1215},
+		{[]int{607, 618, 618, 617, 647, 716, 769, 792}, 3, 5},
+		{input1, 3, 1150},
+	}
+
+	for i, table := range tables {
+		count := CountIncreasingWindow(table.input, table.window)
 
 		if count != table.answer {
 			t.Errorf("Count was incorrect for line %d, got: %d, want: %d.", i, count, table.answer)
