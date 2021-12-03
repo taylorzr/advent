04dfd46a2a8e6d148706465c52865ec71f70b5fa
Zach Taylor <zach@taylorzr.com>
2021-12-03 15:17:18 -0500

    Finish 3, track filtered lists by index instead of creating new lists

--- 3.go
+++ 3.go
@@ -20,7 +20,7 @@ OUT:
 				oneCount++
 			}
 
-			if oneCount > len(inputs)/2 {
+			if oneCount >= len(inputs)-oneCount {
 				gamma[i] = '1'
 				epsilon[i] = '0'
 				continue OUT
@@ -46,51 +46,93 @@ OUT:
 	return g * e
 }
 
+type tracker struct {
+	OneCount    int
+	ZeroCount   int
+	OneIndexes  map[int]bool
+	ZeroIndexes map[int]bool
+}
+
+func NewTracker() *tracker {
+	return &tracker{OneIndexes: map[int]bool{}, ZeroIndexes: map[int]bool{}}
+}
+
 func LifeSupport(inputs []string) uint64 {
-	filtered := inputs
+	oxygenIndexes := map[int]bool{}
+	co2Indexes := map[int]bool{}
 
 	for i := 0; i < len(inputs[0]); i++ {
-		oneCount := 0
-		ones := []string{}
-		zeros := []string{}
+		oxygenTracking := NewTracker()
+		co2Tracking := NewTracker()
 
-		for _, input := range filtered {
-			if input[i] == '1' {
-				oneCount++
-				ones = append(ones, input)
-			} else {
-				zeros = append(zeros, input)
+		for j, input := range inputs {
+			// We keep track of indexes we want to include in the results
+			// so only count if the index is being tracked
+			if i == 0 || oxygenIndexes[j] && len(oxygenIndexes) > 1 {
+				if input[i] == '1' {
+					oxygenTracking.OneCount++
+					oxygenTracking.OneIndexes[j] = true
+				} else {
+					oxygenTracking.ZeroCount++
+					oxygenTracking.ZeroIndexes[j] = true
+				}
+			}
+
+			if i == 0 || co2Indexes[j] && len(co2Indexes) > 1 {
+				if input[i] == '1' {
+					co2Tracking.OneCount++
+					co2Tracking.OneIndexes[j] = true
+				} else {
+					co2Tracking.ZeroCount++
+					co2Tracking.ZeroIndexes[j] = true
+				}
 			}
 		}
 
-		// fmt.Printf("i: %d l: %v 1-count: %d length: %d\n", i, filtered, oneCount, len(filtered))
+		// fmt.Printf("i: %d l: %v 1-count: %d length: %d\n", i, oxygens, oneCount, len(oxygens))
 
-		if oneCount >= len(filtered)-oneCount {
-			filtered = ones
-		} else {
-			filtered = zeros
+		if i == 0 || len(oxygenIndexes) > 1 {
+			if oxygenTracking.OneCount >= oxygenTracking.ZeroCount {
+				oxygenIndexes = oxygenTracking.OneIndexes
+			} else {
+				oxygenIndexes = oxygenTracking.ZeroIndexes
+			}
 		}
 
-		if len(filtered) == 1 {
+		if i == 0 || len(co2Indexes) > 1 {
+			if co2Tracking.OneCount >= co2Tracking.ZeroCount {
+				co2Indexes = co2Tracking.ZeroIndexes
+			} else {
+				co2Indexes = co2Tracking.OneIndexes
+			}
+		}
+
+		if len(oxygenIndexes) == 1 && len(co2Indexes) == 1 {
 			break
 		}
 	}
 
-	oxygen := filtered[0]
+	oxygen := ""
+	for i := range oxygenIndexes {
+		oxygen = inputs[i]
+	}
 
-	fmt.Printf("oxygen: %s\n", oxygen)
+	co2 := ""
+	for i := range co2Indexes {
+		co2 = inputs[i]
+	}
+
+	fmt.Printf("oxygen: %s co2: %s\n", oxygen, co2)
 
 	o, err := strconv.ParseUint(oxygen, 2, 32)
 	if err != nil {
 		panic(err)
 	}
 
-	// e, err := strconv.ParseUint(string(epsilon), 2, 32)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	c, err := strconv.ParseUint(co2, 2, 32)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Printf("o: %d\n", o)
-
-	return o
+	return o * c
 }
