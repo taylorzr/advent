55767db3a25813b018a72dbf11df8b8e6d9b8551
Zach Taylor <zach@taylorzr.com>
2021-12-06 12:06:22 -0500

    Challenge 6 part 2

--- 6_test.go
+++ 6_test.go
@@ -11,8 +11,10 @@ func TestSquidReproduction(t *testing.T) {
 		{input6example, 18, 26},
 		{input6example, 80, 5934},
 		{input6, 80, 390011},
+		// These work but take forever, prolly cause go doesn't have tail call optimization
+		// Or I'm missing a better way to solve this
 		// {input6example, 256, 26984457539},
-		// {input6, 256, 0},
+		// {input6, 256, 1746710169834},
 	}
 
 	for i, table := range tables {
@@ -23,3 +25,12 @@ func TestSquidReproduction(t *testing.T) {
 		}
 	}
 }
+
+/*
+$ go test -run TestSquidReproduction -timeout 99999s
+--- FAIL: TestSquidReproduction (5158.28s)
+    6_test.go:22: Result was incorrect for line 3, expected: 0, got: 1746710169834.
+FAIL
+exit status 1
+FAIL    github.com/taylorzr/advent      5158.651s
+*/
