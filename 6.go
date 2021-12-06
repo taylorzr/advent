69f125ae28de7c60b94180beac503d26d941fa8a
Zach Taylor <zach@taylorzr.com>
2021-12-06 13:39:44 -0500

    6 part 1 iterative

--- 6.go
+++ 6.go
@@ -17,3 +17,26 @@ func births(daysUntil, remainingDays int) int {
 
 	return births(7, remainingDays) + births(9, remainingDays)
 }
+
+func iBirths(input []int, remainingDays int) int {
+	fishes := make([]int, len(input))
+	copy(fishes, input)
+
+	for i := remainingDays; i > 0; i-- {
+		newFishes := []int{}
+
+		for i, fish := range fishes {
+			if fish == 0 {
+				newFishes = append(newFishes, 8)
+				fishes[i] = 6
+			} else {
+				fishes[i] -= 1
+			}
+		}
+
+		// fmt.Printf("%v %v\n", fishes, newFishes)
+		fishes = append(fishes, newFishes...)
+	}
+
+	return len(fishes)
+}
--- 6_test.go
+++ 6_test.go
@@ -2,7 +2,7 @@ package advent
 
 import "testing"
 
-func TestSquidReproduction(t *testing.T) {
+func TestSquidReproductionRecursive(t *testing.T) {
 	tables := []struct {
 		daysUntil     []int
 		remainingDays int
@@ -19,6 +19,28 @@ func TestSquidReproduction(t *testing.T) {
 
 	for i, table := range tables {
 		result := allBirths(table.daysUntil, table.remainingDays)
+
+		if result != table.answer {
+			t.Errorf("Result was incorrect for line %d, expected: %d, got: %d.", i, table.answer, result)
+		}
+	}
+}
+
+func TestSquidReproductionIterative(t *testing.T) {
+	tables := []struct {
+		daysUntil     []int
+		remainingDays int
+		answer        int
+	}{
+		{input6example, 18, 26},
+		{input6example, 80, 5934},
+		{input6, 80, 390011},
+		// {input6example, 256, 26984457539},
+		// {input6, 256, 1746710169834},
+	}
+
+	for i, table := range tables {
+		result := iBirths(table.daysUntil, table.remainingDays)
 
 		if result != table.answer {
 			t.Errorf("Result was incorrect for line %d, expected: %d, got: %d.", i, table.answer, result)
