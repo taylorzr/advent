4cdf3ef8e2f1f201c51b3e2373882d6003b3ccf8
Zach Taylor <zach@taylorzr.com>
2022-12-04 22:46:28 -0500

    haha can check start and end dont need a range between

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,6 @@
 open Format
 open Advent
 
-module SI = Set.Make(Int);;
-
-let range x y = 
-  let rec aux acc start stop =
-    if start == (stop + 1) then
-      acc
-    else 
-      aux (acc @ [start]) (start+1) stop
-  in aux [] x y
-;;
-
-
 let load input = 
   Str.split (Str.regexp "\n") input
   |> List.map (fun line ->
@@ -21,34 +9,34 @@ let load input =
       Str.split (Str.regexp "-") part
       |> (fun part ->
           match part with
-          | [x ; y] -> range (int_of_string x) (int_of_string y)
+          | [x ; y] -> (int_of_string x, int_of_string y)
           | _ -> failwith "taco"
       )
     )
   )
 ;;
 
-let contains = function
-  | [ x ; y ] -> (
-    let shorter = min (List.length x) (List.length y) in
-    let common = SI.inter (SI.of_list x) (SI.of_list y) in
-    if (SI.cardinal common) == shorter then 1 else 0
-  )
+let surrounds = function
+  | [(x, y) ; (x', y')] ->
+    if (x >= x' && y <= y')
+    || (x' >= x && y' <= y) 
+    then 1 else 0
   | _ -> failwith "oops"
 ;;
 
 let overlaps = function
-  | [ x ; y ] -> (
-    let common = SI.inter (SI.of_list x) (SI.of_list y) in
-    if (SI.elements common |> List.length) > 0 then 1 else 0
-  )
+  | [(x, y) ; (x', y')] ->
+    if (x >= x' && x <= y')
+      || (y >= x' && y <= y')
+      || (x' >= x && x' <= y)
+      || (y' >= x && y' <= y)
+    then 1 else 0
   | _ -> failwith "oops"
 ;;
 
-load d4sample |> List.map contains |> List.fold_left (+) 0 |> printf "%d\n";;
-load d4input |> List.map contains |> List.fold_left (+) 0 |> printf "%d\n";;
 
-load d4sample |> List.map overlaps |> List.fold_left (+) 0 |> printf "%d\n";;
+load d4sample |> List.map surrounds |> List.fold_left (+) 0 |> printf "%d\n";;
+load d4input |> List.map surrounds |> List.fold_left (+) 0 |> printf "%d\n";;
+
+load d4sample |> List.map overlaps |> List.fold_left (+) 0 |> printf "%d\n";; 
 load d4input |> List.map overlaps |> List.fold_left (+) 0 |> printf "%d\n";;
-
-
